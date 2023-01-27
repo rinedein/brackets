01b0b5040ab664b4e2737bd75f9eeff251934e96
rinedein <aeternam.hiberna@gmail.com>
2023-01-28 02:20:17 +0600

    edit brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if(str.length % 2 !== 0) return false;
+    for(let n = 0; n < str.length * 40; n++) {
+        bracketsConfig.map(n => {
+            const b = n.join('');
+            if(str.includes(b)) str = str.replace(b,'');
+            return b;
+        });
+    }
+
+    if (str.length) return false
+    else return true
 }
